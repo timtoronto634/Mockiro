18b8e6a1d6854518c21e3476449b1b49c8516f8d
timtoronto634 <tim225x@gmail.com>
2023-05-02 16:34:07 +0900

    Feat extract function to another file

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+export const App = () => {
+    return <div>Hello, React!</div>;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const App = () => {
-  return <div>Hello, React!</div>;
-};
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {App} from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
