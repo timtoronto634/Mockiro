2a0040339d2930ae0abf210e1715b5fc31e9476e
timtoronto634 <tim225x@gmail.com>
2023-05-03 16:54:40 +0900

    Fix type error for experimental feature
    https://github.com/DefinitelyTyped/DefinitelyTyped/issues/43848

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
