e0e90ae5b4bb3d82026de12362ef5db6b8f6f4d9
timtoronto634 <tim225x@gmail.com>
2023-05-10 23:20:27 +0900

    Fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { MindMapBox } from './components/MindMapBox';
 import { MindMapManager } from './components/MindMapManger';
 
 export const App = () => {
   return (
     <div className="App">
       <h1>Mind Map</h1>
-      {/* <MindMapBox /> */}
       <MindMapManager />
     </div>
   );
--- src/components/MindMapManger.tsx
+++ src/components/MindMapManger.tsx
@@ -1,25 +1,27 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 // import { EditableBox } from './EditableBox';
-import {MindMapBox}from './MindMapBox';
+import { MindMapBox } from './MindMapBox';
 
 export const MindMapManager = () => {
   const addNode = () => {
     console.log('addNode');
-  }
+  };
   const rootNode = MindMapBox(addNode);
   const [columns, setColumns] = useState<JSX.Element[]>([rootNode]);
   const tmpButton = () => {
-    columns.push(MindMapBox(addNode))
-    console.log(columns)
-    setColumns(columns)
-  }
-
+    columns.push(MindMapBox(addNode));
+    console.log(columns);
+    setColumns(columns);
+  };
 
   return (
     <div className="Nodes">
-      {columns.map((column, index) => {return <div key={index}>{column}</div>} )}
-      <button className="tmp-button" onClick={tmpButton}>+</button>
-
+      {columns.map((column, index) => {
+        return <div key={index}>{column}</div>;
+      })}
+      <button className="tmp-button" onClick={tmpButton}>
+        +
+      </button>
     </div>
-  )
-}
+  );
+};
