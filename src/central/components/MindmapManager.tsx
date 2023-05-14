8a56cd3ee201bea0e6dfbeb543b9fa3cdb5e2521
timtoronto634 <tim225x@gmail.com>
2023-05-14 15:09:59 +0900

    Feat WIP: add move function

--- src/central/components/MindmapManager.tsx
+++ src/central/components/MindmapManager.tsx
@@ -1,16 +1,56 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { MindmapNode } from './MindmapNode';
 
+interface Node {
+  id: string;
+  parentId: string | null;
+}
+
 export const MindmapManager: React.FC = () => {
-  // const [selectedNodeId, setSelectedNodeId] = useState<number | null>(null);
+  const [nodes, setNodes] = useState<Node[]>([{ id: 'root', parentId: null }]);
 
-  // const handleNodeSelection = (id: number) => {
-  //   setSelectedNodeId(id);
-  // };
+  const moveNode = useCallback((nodeId: string, newParentId: string) => {
+    setNodes((prevNodes) => {
+      const nodeIndex = prevNodes.findIndex((node) => node.id === nodeId);
+      if (nodeIndex === -1) {
+        console.error(`Node with ID '${nodeId}' not found`);
+        return prevNodes;
+      }
 
-  return (
-    <div className="App">
-      <MindmapNode level={0} />
-    </div>
+      const updatedNode: Node = {
+        ...prevNodes[nodeIndex],
+        parentId: newParentId,
+      };
+
+      const updatedNodes = [...prevNodes];
+      updatedNodes[nodeIndex] = updatedNode;
+      console.log(updatedNodes);
+      return updatedNodes;
+    });
+  }, []);
+
+  const addNode = (id: string, parentId: string | null) => {
+    setNodes((prevNodes) => {
+      console.log(prevNodes);
+      return [...prevNodes, { id: id, parentId: parentId }];
+    });
+  };
+
+  const renderNodes = useCallback(
+    (parentId: string | null) =>
+      nodes
+        .filter((node) => node.parentId === parentId)
+        .map((node) => (
+          <MindmapNode
+            key={node.id}
+            level={0}
+            id={node.id}
+            moveNode={moveNode}
+            addNode={addNode}
+          />
+        )),
+    [nodes, moveNode]
   );
+
+  return <div className="Mindmap-Manager">{renderNodes(null)}</div>;
 };
--- src/central/components/MindmapNode.tsx
+++ src/central/components/MindmapNode.tsx
@@ -2,39 +2,81 @@ import React, { useState } from 'react';
 
 interface MindmapNodeProps {
   level: number;
+  id: string;
+  moveNode: (nodeId: string, newParentId: string) => void;
+  addNode: (id: string, parentId: string | null) => void;
 }
 
 interface Node {
-  id: number;
+  level: number;
+  id: string;
   text: string;
+  moveNode: (nodeId: string, newParentId: string) => void;
 }
 
-export const MindmapNode: React.FC<MindmapNodeProps> = ({ level }) => {
+export const MindmapNode: React.FC<MindmapNodeProps> = ({
+  level,
+  id,
+  moveNode,
+  addNode,
+}) => {
   // const [leftNode, setLeftNode] = useState<Node | null>(null);
   const [rightNodes, setRightNodes] = useState<Node[]>([]);
 
   const addRightNode = () => {
+    const newId = level * 10000 + generateRandomId();
     const newNode: Node = {
-      id: rightNodes.length,
+      id: newId,
+      level: level + 1,
       text: '',
+      moveNode: moveNode,
     };
+    addNode(newId, id);
     setRightNodes([...rightNodes, newNode]);
+  };
+
+  const moveToRoot = () => {
+    moveNode(id, 'root');
   };
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       <div style={{ display: 'flex', flexDirection: 'row' }}>
         <input type="text" />
+        <button onClick={moveToRoot}>move to root</button>
         <button onClick={addRightNode}>+</button>
       </div>
 
       <div style={{ display: 'flex', flexDirection: 'column' }}>
         {rightNodes.map((node) => (
-          <div style={{ display: 'flex', flexDirection: 'row' }} key={node.id}>
-            <MindmapNode key={node.id} level={level + 1} />
+          <div
+            style={{ display: 'flex', flexDirection: 'row' }}
+            key={node.id + 'wrap'}
+          >
+            <MindmapNode
+              key={node.id}
+              level={level + 1}
+              id={node.id}
+              // parentId={node.parentId}
+              moveNode={node.moveNode}
+              addNode={addNode}
+            />
           </div>
         ))}
       </div>
     </div>
   );
 };
+
+function generateRandomId(length = 10): string {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength: number = characters.length;
+  let result = '';
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
